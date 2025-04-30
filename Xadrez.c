c7667483bec5c000b8d950fd34623297104f70de
Daniel Bruno <daniel.bruno.dev19@gmail.com>
2025-04-30 06:38:18 +0000

    Atualização nível mestre.

--- Xadrez.c
+++ Xadrez.c
@@ -1,5 +1,67 @@
 #include <stdio.h>
 
+void moverTorre(int casas){
+    if(casas > 0){
+    printf("Direita.\n");
+    moverTorre(casas - 1);
+    }
+}
+
+void moverRainha(int casas){
+    if(casas > 0){
+    printf("Esquerda.\n");
+    moverRainha(casas - 1);
+    }
+}
+
+void moverBispo(int casas){
+    int b = 1;
+    while(b <= casas){
+        int B;
+        printf("Cima.\n");
+        b++;
+        for(B = casas; B >= casas; B--){
+            printf("Direita.\n");
+        }
+    }
+}
+
+void moverCavalo(){
+    int c = 1;
+    do{
+        for(int C = 1; C < 3; C++){
+            printf("Cima.\n");
+        }
+        printf("Direita.\n");
+        c++;
+    } while(c < 2);
+}
+
+int main(){
+
+    moverTorre(5);
+    printf("\n");
+    moverRainha(8);
+    printf("\n");
+    moverBispo(5);
+    printf("\n");
+    moverCavalo();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 int main(){
 
     int r = -1, b = 1, c = 1;
@@ -34,3 +96,4 @@ int main(){
 
     return 0;
 }
+    */
